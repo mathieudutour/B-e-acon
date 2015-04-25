17a2a791aa5de68b9b60ce20ece952bb69ea8ee0
Mathieu Dutour <mathieu@dutour.me>
2015-04-25 16:24:29 +0100

    signup/ login shop

--- client/controllers/landing.js
+++ client/controllers/landing.js
@@ -1,5 +1,14 @@
+Template.landing.helpers({
+  shops: function() {
+    Meteor.users.find({isShop: true});
+  }
+});
+
 Template.landing.events({
   "click #login": function(e, t) {
     Router.goToPage(Router.Page.LOGIN);
+  },
+  "click #login-shop": function(e, t) {
+    Router.goToPage(Router.Page.SHOP_LOGIN);
   }
 });
