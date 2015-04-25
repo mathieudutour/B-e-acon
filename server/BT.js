4a77439d632777d6bd285b4cc98717947e7d0b40
jakubbaron <jakubaron@gmail.com>
2015-04-25 15:52:04 +0100

    BraintTree - Meteor
    Added files needed to run this with our services

---
+++ server/BT.js
@@ -0,0 +1,46 @@
+var gateway;
+
+Meteor.startup(function () {
+  var braintree = Meteor.npmRequire('braintree');
+  gateway = braintree.connect({
+    environment: braintree.Environment.Sandbox,
+    publicKey: Meteor.settings.BT_PUBLIC_KEY,
+    privateKey: Meteor.settings.BT_PRIVATE_KEY,
+    merchantId: Meteor.settings.BT_MERCHANT_ID
+  });
+});
+
+Meteor.methods({
+  getClientToken: function (clientId) {
+    var generateToken = Meteor.wrapAsync(gateway.clientToken.generate, gateway.clientToken);
+    var options = {};
+
+    if (clientId) {
+      options.clientId = clientId;
+    }
+
+    var response = generateToken(options);
+
+    return response.clientToken;
+  },
+  createTransaction: function (data) {
+    var transaction = Meteor.wrapAsync(gateway.transaction.sale, gateway.transaction);
+    // this is very naive, do not do this in production!
+    var amount = parseInt(data.quantity, 10) * data.price;
+      console.log(data);
+    var response = transaction({
+      amount: amount,
+      paymentMethodNonce: data.nonce,
+      customer: {
+        firstName: data.firstName,
+        lastName: data.lastName
+      }
+    });
+
+    // ...
+    // perform a server side action with response
+    // ...
+
+    return response;
+  }
+});
