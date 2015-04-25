e7ed9f813fc1b18f7b5a1331d69ae04c2964b428
chabrowa <elessarin@gmail.com>
2015-04-25 21:40:07 +0100

    working form

--- client/controllers/shop_config.js
+++ client/controllers/shop_config.js
@@ -5,11 +5,18 @@ Template.shop_config.onRendered(function () {
       $(Session.get("currentPicker")).css("background-color", color);
     }
   });
+  
+  $('#colorBackground').css("background-color", Meteor.user().profile.info.backgroundColor);
+  $('#colorMain').css("background-color", Meteor.user().profile.info.mainColor);
+  $('#colorText').css("background-color", Meteor.user().profile.info.textColor);
 });
 
 Template.shop_config.events({
   "click #colorBackground": function (e, t) {
     Session.set('currentPicker', "#colorBackground");
+  },
+  "change #colorBackground": function (e, t) {
+    $('#colorBackground').css("background-color", e.currentTarget.value);
   },
   "click #colorMain": function (e, t) {
     Session.set('currentPicker', "#colorMain");
@@ -22,6 +29,31 @@ Template.shop_config.events({
     Meteor.users.update(Meteor.userId(), {$push: {"profile.sections":
           {name: "", description: "", items: [], _id: new Mongo.ObjectID()}}});
   },
+  "submit form": function (e, t) {
+    event.preventDefault();
+    
+    Meteor.users.update(Meteor.userId(), {$set: {"profile.name": t.find("#companyName").value}});
+    
+    Meteor.users.update(Meteor.userId(), {$set: {"profile.info": 
+          {motto: t.find('#companyMotto').value, backgroundColor: t.find('#colorBackground').value, mainColor: t.find('#colorMain').value,              textColor: t.find('#colorText').value, beacon: t.find('#beaconID').value}}});
+    
+    var sections = Meteor.user().profile.sections;
+    sections.map(function(section){
+      var idSection = section._id._str;
+      
+      section.name = t.find('#name_'+idSection).value;
+      section.description = t.find('#description_'+idSection).value;
+      section.items = section.items.map(function(item){
+        var idItem = item._id._str;
+        item.name = t.find('#name_'+idItem).value;
+        item.description = t.find('#description_'+idItem).value;
+        item.price = t.find('#price_'+idItem).value;
+        return item;
+      })
+      return section;
+    });
+    Meteor.users.update(Meteor.userId(), {$set: {"profile.sections": sections}});
+  },
 });
 
 Template.shop_config.helpers({
@@ -31,10 +63,26 @@ Template.shop_config.helpers({
   sections: function () {
     return Meteor.user().profile.sections;
   },
+  companyMotto: function() {
+    return Meteor.user().profile.info.motto;
+  },
+  beacon: function() {
+    return Meteor.user().profile.info.beacon;
+  },
+  colorBackground: function() {
+    return Meteor.user().profile.info.backgroundColor;
+  },
+  colorMain: function() {
+    return Meteor.user().profile.info.mainColor;
+  },
+  colorText: function() {
+    return Meteor.user().profile.info.textColor;
+  },
+  
 });
 
 Template.menuList.events({
-  "click #nextItem": function (e, t) {
+  "click .nextItem": function (e, t) {
     e.preventDefault();
     var newSections = Meteor.user().profile.sections.map(function(section){
       if(EJSON.equals(section._id, t.data._id)) {
@@ -44,7 +92,7 @@ Template.menuList.events({
     });
     Meteor.users.update(Meteor.userId(), {$set: {"profile.sections": newSections}});
   },
-  "click #removeSection": function(e, t) {
+  "click .removeSection": function(e, t) {
     e.preventDefault();
     var newSections = Meteor.user().profile.sections.filter(function(section){
       return !EJSON.equals(section._id, t.data._id);
