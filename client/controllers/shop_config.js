e4e0aba81a39f005499f498b37ad1dfc19c17a07
chabrowa <elessarin@gmail.com>
2015-04-25 16:47:47 +0100

    menu start

--- client/controllers/shop_config.js
+++ client/controllers/shop_config.js
@@ -1,5 +1,5 @@
-Template.shop_config.onRendered( function() {
-  $('#colorpicker').farbtastic(function(color){
+Template.shop_config.onRendered(function () {
+  $('#colorpicker').farbtastic(function (color) {
     if (Session.get("currentPicker")) {
       $(Session.get("currentPicker")).val(color);
       $(Session.get("currentPicker")).css("background-color", color);
@@ -8,13 +8,26 @@ Template.shop_config.onRendered( function() {
 });
 
 Template.shop_config.events({
-  "click #colorBackground": function(e, t) {
+  "click #colorBackground": function (e, t) {
     Session.set('currentPicker', "#colorBackground");
   },
-  "click #colorMain": function(e, t) {
+  "click #colorMain": function (e, t) {
     Session.set('currentPicker', "#colorMain");
   },
-  "click #colorText": function(e, t) {
+  "click #colorText": function (e, t) {
     Session.set('currentPicker', "#colorText");
-  }
+  },
+  "click #nextSection": function (e, t) {
+    Meteor.users.update({_id: Meteor.userId()}, {"profile.sections": {$push: {name: "", description: ""}}})
+  },
+});
+
+Template.shop_config.helpers({
+  companyName: function () {
+    //return Meteor.user().profile.name;
+    return "nananana batman"
+  },
+  sections: function () {
+    return Meteor.user().profile.sections;
+  },
 });
