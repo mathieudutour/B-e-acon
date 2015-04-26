8bdb12a2c3c67480197e5be1207025e85440c07a
jakubbaron <jakubaron@gmail.com>
2015-04-26 10:03:09 +0100

    Confirmation number added

--- client/controllers/confirmation.js
+++ client/controllers/confirmation.js
@@ -1,9 +1,11 @@
 Template.confirmation.helpers({
   confNumber: function () {
-    var conf = CartItems.find({
-      session: Meteor.default_connection._lastSessionId
-    }).fetch();
-//    return conf._id;
-    return "nananana BATMAN";
+    var conf = CartItems.findOne({
+      "session": Meteor.default_connection._lastSessionId
+    });
+//    console.log(conf);
+//    console.log(Meteor.default_connection._lastSessionId);
+    return conf._id;
+//    return "nananana BATMAN";
   }
 });
