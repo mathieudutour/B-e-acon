ac565f68d8f98bfa5aca1372ad71bf06a6591dbc
jakubbaron <jakubaron@gmail.com>
2015-04-26 03:18:53 +0100

    Changed checkout, payments need fix

--- client/controllers/cart.js
+++ client/controllers/cart.js
@@ -3,22 +3,46 @@ Template.cart.helpers({
     var shopCart = [];
     var total = 0;
 
-    CartItems.find({session:Meteor.default_connection._lastSessionId})
+    CartItems.find({
+        session: Meteor.default_connection._lastSessionId
+      })
       .forEach(function (cartitem) {
-      cartitem.price = (parseFloat(cartitem.product.price) * cartitem.quantity);
-      total += cartitem.price;
-      shopCart.push(cartitem);
-    });
+        cartitem.price = (parseFloat(cartitem.product.price) * cartitem.quantity);
+        total += cartitem.price;
+        shopCart.push(cartitem);
+      });
 
     shopCart.subtotal = total;
     shopCart.tax = shopCart.subtotal * 0.06;
     shopCart.total = shopCart.subtotal + shopCart.tax;
     return shopCart;
+  },
+  'checkoutActive':function(){
+    return Session.get('checkoutActive');
+  },
+  'payNow':function(){
+    return Session.get('payNow');
   }
 });
 
 Template.cart.events({
   'click .removeci': function () {
-    CartItems.remove({_id: this._id});
+    CartItems.remove({
+      _id: this._id
+    });
+  },
+  'click .checkout__button': function (ev) {
+    ev.preventDefault();
+    Session.set('checkoutActive',true);
+
+  },
+  'click .checkout__cancel': function (ev) {
+    ev.preventDefault();
+    Session.set('checkoutActive',false);
+    Session.set('payNow', false);
+  },
+  'click .pay-now-btn': function(ev) {
+    ev.preventDefault();
+    Session.set('payNow',true);
   }
 });
