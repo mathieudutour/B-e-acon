d0c826052a78782aea002d52f95640c2268f7520
Mathieu Dutour <mathieu@dutour.me>
2015-04-26 11:59:24 +0100

    hot fix

--- client/controllers/landing.js
+++ client/controllers/landing.js
@@ -1,16 +1,15 @@
 Template.landing.helpers({
   shops: function() {
-//    return Meteor.users.find({'profile.isShop': true, "profile.info.beacon":
-//                       {$in: JSON.parse(Session.get('beaconIds'))
-//                        .map(function(id){
-//        return "" + id;
-//      })}});
     var beacons = Session.get('beaconIds');
-    console.log(beacons);
-    beacons = JSON.parse(beacons);
-    beacons = beacons.map(function(id){return "" + id;});
-    return Meteor.users.find({'profile.isShop': true,
-      "profile.info.beacon": {$in: beacons}});
+    if (beacons) {
+      console.log(beacons);
+      beacons = JSON.parse(beacons);
+      beacons = beacons.map(function(id){return "" + id;});
+      return Meteor.users.find({'profile.isShop': true,
+        "profile.info.beacon": {$in: beacons}}).fetch();
+    } else {
+      return null;
+    }
   }
 });
 
