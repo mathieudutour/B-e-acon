3b017c0caa4bfb3c42f7c12cb8f859aa4f0c24df
jakubbaron <jakubaron@gmail.com>
2015-04-26 06:19:26 +0100

    Confirmation
    Will fix it after 8

---
+++ client/controllers/confirmation.js
@@ -0,0 +1,8 @@
+Template.confirmation.helpers({
+  confNumber: function () {
+    var conf = CartItems.find({
+      session: Meteor.default_connection._lastSessionId
+    }).fetch();
+    return conf._id;
+  }
+});
